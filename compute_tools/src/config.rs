12c2fc96834f59604b8ade5b9add28f1dce41ec6
洛斐 <luofei.zlf@alibaba-inc.com>
2024-06-26 16:56:55 +0800

    fix(compute_tools): write_postgres_conf missing newline

--- compute_tools/src/config.rs
+++ compute_tools/src/config.rs
@@ -109,7 +109,7 @@ pub fn write_postgres_conf(
                 vartype: "enum".to_owned(),
             };
 
-            write!(file, "{}", opt.to_pg_setting())?;
+            writeln!(file, "{}", opt.to_pg_setting())?;
         }
     }
 
