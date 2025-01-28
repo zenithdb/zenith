a459b49424caf7a30b9500d84be71f2ed8f131e0
Vlad Lazar <vlad@neon.tech>
2025-01-28 15:12:42 +0100

    sample rate back to 0

--- pageserver/src/page_service.rs
+++ pageserver/src/page_service.rs
@@ -870,7 +870,7 @@ impl PageServerHandler {
                 let key = rel_block_to_key(req.rel, req.blkno);
 
                 fn sampled() -> bool {
-                    const SAMPLE_RATE: f32 = 0.01;
+                    const SAMPLE_RATE: f32 = 0.00;
                     if SAMPLE_RATE <= 0.0 {
                         false
                     } else if SAMPLE_RATE >= 1.0 {
